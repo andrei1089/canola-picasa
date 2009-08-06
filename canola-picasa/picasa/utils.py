e5376be916d8208851e418e65cd4558a9218a1aa
andrei1089 <andrei.mirestean@gmail.com>
2009-08-06 17:16:09 +0300

    Improvements in download_file
    Download again the file in case of an error

--- canola-picasa/picasa/utils.py
+++ canola-picasa/picasa/utils.py
@@ -4,6 +4,8 @@ import os.path
 
 from math import cos, pi
 
+import ecore
+
 from terra.core.manager import Manager
 from terra.ui.grid import CellRenderer
 from terra.ui.base import EdjeWidget
@@ -21,14 +23,27 @@ log = logging.getLogger("canola.plugins.images.utils")
 
 def download_file(model, path, url, callback_exists=None, \
       callback_downloaded=None, callback_in_progress=None, attr="downloader"):
+
+    def download_again(url, path, model):
+        log.error("Download again")
+        downloader = download_mger.add(url, path)
+        downloader.on_finished_add(download_finished)
+        downloader.start(True)
+        model.__setattr__(attr, downloader)
+
+
     def download_finished(exception, mimetype):
         if exception is None:
             log.debug("Finished download for %s, calling callback" % path)
             callback_downloaded()
         else:
-            log.error("Error while downloading file %s from %s, error %s" % \
+            log.error("Error while downloading file %s from %s, error %s, trying to download again" % \
                                                         (path, url, exception))
-
+            try:
+                os.unlink(path)
+            except:
+                log.info("Trying to delete nonexisting file")
+            ecore.timer_add(0.5, download_again, url, path, model)
     def download_in_progress(exception, mimetype):
         if exception is None:
             callback_in_progress()
